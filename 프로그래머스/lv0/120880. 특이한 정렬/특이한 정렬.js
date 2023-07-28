7087da300ff7127ef7c3c2837c4cf7b51ed15910
blcklamb <whenucan35@gmail.com>
2023-07-28 20:15:15 +0900

    [level 0] Title: 특이한 정렬, -우수코드추가

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,8 +1,15 @@
 function solution(numlist, n) {
-    const sortedList = numlist.map((ele) => ele - n).sort((a, b) => {
-        const A = Math.abs(a)
-        const B = Math.abs(b)
-        return (A < B) ?  -1 : (A > B) ?  1 : ( a < 0 ) ?  1 :  -1
-    })
-    return sortedList.map((ele) => ele + n);
+  const sortedList = numlist
+    .map((ele) => ele - n)
+    .sort((a, b) => {
+      const A = Math.abs(a);
+      const B = Math.abs(b);
+      return A < B ? -1 : A > B ? 1 : a < 0 ? 1 : -1;
+    });
+  return sortedList.map((ele) => ele + n);
+}
+
+// 우수 코드
+function solution(numlist, n) {
+  return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
 }

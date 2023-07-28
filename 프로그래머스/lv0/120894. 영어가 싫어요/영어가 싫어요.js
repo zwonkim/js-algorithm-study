32cfbd966a31facda6733781b77a27fdab984aa4
blcklamb <whenucan35@gmail.com>
2023-07-28 20:15:46 +0900

    [level 0] Title: 영어가 싫어요, -직접코드추가

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,9 +1,58 @@
 function solution(numbers) {
-    const replaceToNum = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    
-    replaceToNum.forEach((numString, num) => {
-        numbers = numbers.replaceAll(numString, num)
-    })
-    
-    return Number(numbers);
+  const replaceToNum = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  replaceToNum.forEach((numString, num) => {
+    numbers = numbers.replaceAll(numString, num);
+  });
+
+  return Number(numbers);
 }
+
+```
+'평균 시간: 0.070ms'
+'평균 메모리: 33.433MB'
+'최고 시간: 0.09ms'
+'최저 시간: 0.05ms'
+'최고 메모리: 33.5MB'
+'최저 메모리: 33.4MB'
+'시간 표준 편차: 0.027'
+'메모리 표준 편차: 11.172'
+```;
+
+function solution(numbers) {
+  const answer = numbers
+    .replaceAll("one", 1)
+    .replaceAll("two", 2)
+    .replaceAll("three", 3)
+    .replaceAll("four", 4)
+    .replaceAll("five", 5)
+    .replaceAll("six", 6)
+    .replaceAll("seven", 7)
+    .replaceAll("eight", 8)
+    .replaceAll("nine", 9)
+    .replaceAll("zero", 0);
+
+  return Number(answer);
+}
+```
+'평균 시간: 0.040ms'
+'평균 메모리: 33.456MB'
+'최고 시간: 0.04ms'
+'최저 시간: 0.04ms'
+'최고 메모리: 33.5MB'
+'최저 메모리: 33.4MB'
+'시간 표준 편차: 0.013'
+'메모리 표준 편차: 11.140'
+
+```;

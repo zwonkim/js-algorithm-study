c5357d5338247366c0b4c563551c10206e38a4d4
nimgnas <sangminyn@gmail.com>
2023-07-31 08:55:48 +0900

    [level 0] Title: 공 던지기, -직접코드추가

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -13,3 +13,15 @@ function solution(numbers, k) {
 
   return (answer = numbers[0]);
 }
+
+// 코드추가
+function solution(numbers, k) {
+  let idx = 0;
+  for (let i = 1; i < k; i++) {
+    idx += 2;
+    if (idx > numbers.length) {
+      idx %= numbers.length;
+    }
+  }
+  return numbers[idx];
+}

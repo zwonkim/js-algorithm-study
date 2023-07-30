c75f4ca66c4bbfb04cc95a01a85459ec1588df61
Sangmin Yoon <sangminyn@gmail.com>
2023-07-31 08:05:17 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,4 +1,12 @@
 function solution(chicken) {
-    var answer = 0;
-    return answer;
+  var answer = 0;
+
+  while (chicken >= 10) {
+    const na = chicken % 10;
+    chicken = Math.floor(chicken / 10);
+    answer += chicken;
+
+    chicken = na + chicken;
+  }
+  return answer;
 }

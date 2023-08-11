8d97da0e7b5f7fc96c8ac5995758ae26f8a4e076
Sangmin Yoon <sangminyn@gmail.com>
2023-08-11 10:58:29 +0900

    [unrated] Title: 과일 장수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,21 +1,13 @@
 function solution(k, m, score) {
   var answer = 0;
   score.sort((a, b) => a - b);
-    
-    console.log(score)
 
   const numOfbox = Math.floor(score.length / m);
 
-  if (score.length % m === 0) {
-    for (let i = 0; i < numOfbox; i++) {
-      answer += score[i * m] * m;
-    }
-  } else {
-    for (let i = 0; i < numOfbox; i++) {
-      answer += score[i * m + (score.length - m * numOfbox)] * m
-    }
+  for (let i = 0; i < numOfbox; i++) {
+    if (score.length % m === 0) answer += score[i * m] * m;
+    else answer += score[i * m + (score.length - m * numOfbox)] * m;
   }
 
   return answer;
 }
-

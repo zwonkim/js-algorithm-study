c7e89a21e4dae10a2d0beb65d17dc326ab4129c4
Sangmin Yoon <sangminyn@gmail.com>
2023-08-21 13:03:16 +0900

    [unrated] Title: 기사단원의 무기, Time: 5.43 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,22 +1,4 @@
 function solution(number, limit, power) {
-  let arr = []
-    
-  for (let i = 1; i <= number; i++) {
-    const divisorArr = []
-    for (let j = 1; j <= Math.floor(Math.sqrt(i)); j++) {
-      if (i % j === 0) divisorArr.push(j)
-    }
-    
-    const n = divisorArr.length
-    for(let j = 0; j < n; j++) {
-        if(i / divisorArr[j] !== divisorArr[j]) divisorArr.push(i / divisorArr[j])
-    }
-    
-      arr.push(divisorArr.length)
-  }
-
-  return arr.reduce((a, c) => {
-    if (c > limit) return a + power;
-    else return a + c;
-  }, 0);
+    var answer = 0;
+    return answer;
 }

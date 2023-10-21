d332c14abc1c0278ba610060922a941d911c3c81
Chaejung Kim <whenucan35@gmail.com>
2023-10-21 11:18:26 +0900

    [10월 3주차] (#64)
    * [level unrated] Title: 문자열의 뒤의 n글자, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub
    
    * [level 2] Title: 마법의 엘리베이터, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub
    
    * [level unrated] Title: 문자열의 뒤의 n글자, --직접 코드추가
    
    * [10월 3주차] 폴더 이동

---
+++ KimChaeJung/프로그래머스/lv0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
@@ -0,0 +1,29 @@
+function solution(my_string, n) {
+  return my_string.slice(-n, my_string.length);
+}
+
+```
+'평균 시간: 0.027ms'
+'평균 메모리: 33.417MB'
+'최고 시간: 0.04ms'
+'최저 시간: 0.02ms'
+'최고 메모리: 33.5MB'
+'최저 메모리: 33.3MB'
+'시간 표준 편차: 0.007'
+'메모리 표준 편차: 7.883'
+```;
+
+function solution(my_string, n) {
+  return my_string.substring(my_string.length - n);
+}
+
+```
+'평균 시간: 0.029ms'
+'평균 메모리: 33.417MB'
+'최고 시간: 0.04ms'
+'최저 시간: 0.02ms'
+'최고 메모리: 33.5MB'
+'최저 메모리: 33.3MB'
+'시간 표준 편차: 0.007'
+'메모리 표준 편차: 7.883'
+```;
